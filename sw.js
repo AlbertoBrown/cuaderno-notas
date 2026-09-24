21e595aebc6516f7ba667c33d0e25f9f5e955897
Alberto Moreno Sánchez <61455600+AlbertoBrown@users.noreply.github.com>
2026-09-24 09:33:33 +0200

    Bump PWA cache for editing UX

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "cuaderno-notas-v22";
+const CACHE = "cuaderno-notas-v23";
 const SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=22",
+  "./styles.css?v=23",
   "./manifest.webmanifest",
   "./icon.svg",
-  "./js/app.js?v=22",
+  "./js/app.js?v=23",
   "./js/store.js",
   "./js/sync.js",
   "./js/supabase.js",
